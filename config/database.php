9c7ed2e3f86c49616cc84466f1e3581bc07df282
Bruno Costa <bhcosta90@gmail.com>
2026-03-28 20:51:58 -0300

    chore(database): update env variable for PostgreSQL search_path to DB_SEARCH_PATH

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => env('SEARCH_PATH', 'public'),
+            'search_path' => env('DB_SEARCH_PATH', 'public'),
             'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
